be5eef2a7a5222d60e5e079015065ceca0c688df
leo <leo.opdenplatz@gmail.com>
2022-09-28 14:16:22 +0200

    added level variables and getPointsFromRange function

--- nutriscore.go
+++ nutriscore.go
@@ -35,6 +35,16 @@ type NutritionalData struct {
 	IsWater             bool
 }
 
+var energyLevels = []float64{3350, 3015, 2680, 2345, 2010, 1675, 1340, 1005, 670, 335}
+var sugarsLevels = []float64{45, 60, 36, 31, 27, 22.5, 18, 13.5, 9, 4.5}
+var saturatedFattyAcidsLevels = []float64{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
+var sodiumLevels = []float64{900, 810, 720, 630, 540, 450, 360, 270}
+var fibreLevels = []float64{4.7, 3.7, 2.8, 1.9, 0.9}
+var proteinLevels = []float64{8, 6.4, 4.8, 3.2, 1.6}
+
+var energyLevelsBeverage = []float64{270, 240, 210, 180, 150, 120, 90, 60, 30, 0}
+var sugarsLevelsBeverage = []float64{13.5, 12, 10.5, 9, 7.5, 6, 4.5, 3, 1.5, 0}
+
 func (e EnergyKJ) GetPoints(st ScoreType) int {
 
 }
@@ -47,20 +57,28 @@ func (sfa SaturatedFattyAcids) GetPoints(st ScoreType) int {
 
 }
 
-func (so SodiumMilligram) GetPoints (st ScoreType) int {
+func (so SodiumMilligram) GetPoints(st ScoreType) int {
 
 }
 
-func (f FruitsPercent)GetPoints (st ScoreType) int {
+func (f FruitsPercent) GetPoints(st ScoreType) int {
 
 }
 
-func (fg FibreGram) GetPoints (st ScoreType) int {
+func (fg FibreGram) GetPoints(st ScoreType) int {
 
 }
 
-func  (p ProteinGram) GetPoints (st ScoreType) int {
+func (p ProteinGram) GetPoints(st ScoreType) int {
 
+}
+
+func EnergyFromKcal(kcal float64) EnergyKJ {
+	return EnergyKJ(kcal * 4.184)
+}
+
+func SodiumFromSalt(saltMg float64) SodiumMilligram {
+	return SodiumMilligram(saltMg / 2.5)
 }
 
 func GetNutritionalScore(n NutritionalData, st ScoreType) NutritionalScore {
@@ -83,4 +101,14 @@ func GetNutritionalScore(n NutritionalData, st ScoreType) NutritionalScore {
 		Negative:  negative,
 		ScoreType: st,
 	}
+}
+
+func getPointsFromRange(v float64, steps []float64) int {
+	lenSteps := len(steps)
+	for i, l := range steps {
+		if v > l {
+			return lenSteps - i
+		}
+	}
+	return 0
 }
